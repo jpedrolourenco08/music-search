c2f2a7076856a3786739c735cc2ec3dfdb34d779
jpedrolourenco08 <jpedro.lourenco08@gmail.com>
2026-03-18 06:44:59 -0300

    INIT: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '/music-search/src/pages/home.tsx'
+import './App.css'
+
+function App() {
+  const [theme, setTheme] = useState('light')
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+  }, [theme])
+
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+          Alternar tema
+        </button>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Bem-vindo à Home!</h1>
+      <p>Este é o conteúdo da página inicial.</p>
+    </div>
+  )
+}
